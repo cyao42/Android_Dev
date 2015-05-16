a0bf6633cd774c5f08fe1dcafb0b4004a98220f4
Carolyn Yao <cyao42@gmail.com>
2015-05-16 02:00:40 -0400

    Added weather app
    Basic weather app! Some practice with JSON,  Butter Knife API, and app
    layout

---
+++ Stormy/app/src/main/java/carolynyao/stormy/CurrentWeather.java
@@ -0,0 +1,118 @@
+package carolynyao.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by cyao42 on 4/6/2015.
+ */
+public class CurrentWeather {
+
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String mTimeZone) {
+        this.mTimeZone = mTimeZone;
+    }
+
+    private String mTimeZone;
+
+    public String getmIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String mIcon) {
+        this.mIcon = mIcon;
+    }
+    public int getIconId() {
+        int iconId = R.drawable.clear_day;
+        if (mIcon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+        return iconId;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime() * 1000);
+        String timeString = formatter.format(dateTime);
+        return timeString;
+    }
+    public void setTime(long mTime) {
+        this.mTime = mTime;
+    }
+
+    public int getTemperature() {
+        return (int) Math.round(mTemperature);
+    }
+
+    public void setTemperature(double mTemperature) {
+        this.mTemperature = mTemperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double mHumidity) {
+        this.mHumidity = mHumidity;
+    }
+
+    public int getPrecipChance(){
+        double precipPercentage = mPrecipChance * 100;
+        return (int) Math.round(precipPercentage);
+    }
+
+    public void setPrecipChance(double mPrecipChance) {
+        this.mPrecipChance = mPrecipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String mSummary) {
+        this.mSummary = mSummary;
+    }
+}
